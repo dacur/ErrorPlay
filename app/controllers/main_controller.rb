9a6a246f9342029079c79efea2913c12df9db14f
David Curtis <dacur7@gmail.com>
2014-05-06 10:52:06 -0400

    Added to main controller.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,16 +3,24 @@ class MainController < ApplicationController
 
 def giraffe
 	d = params[:penguin].to_i
-	begin 
+	begin
 		if d > 5
 			d / 1
 		else
-			d / 0
+			if d == 4
+				puts "hi"
+				raise ArgumentError "Boom" #this is not working
+			else 
+				d / 0
+			end
 		end
+	rescue ArgumentError => e #this is not working
+		puts "argument error" #this is not working
+
 	rescue => e
 		puts 'Error'
 	else
-		puts "Else " + d.to_s
+		puts "Else " + d.to_s #have to use .to_s b/c i am concatenating w/ "Else", which is a string
 	ensure
 		puts 'All Done!'
 	end
